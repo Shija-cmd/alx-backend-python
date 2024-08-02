d77f98441bbf06520774483b1bb791ccdc469faf
Shija-cmd <shijajuma@protonmail.com>
2024-08-02 19:13:30 +0000

    update

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type annotated function concat
+    """
+    return str1 + str2
