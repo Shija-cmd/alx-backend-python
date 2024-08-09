01cdd535e8c2517d3262a9d02a1cd48ae13487d5
Shija-cmd <shijajuma@protonmail.com>
2024-08-09 12:40:11 +0000

    update

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""spawn wait_random n times with the specified max_delay"""
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n should return the list of all the delays (float values)."""
+    futures = [wait_random(max_delay) for _ in range(n)]
+    futures = asyncio.as_completed(futures)
+    delays = [await future for future in futures]
+    return delays
