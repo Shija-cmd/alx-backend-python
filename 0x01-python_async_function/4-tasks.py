9613246fc8797bca5d472d159f1090e4b9ef1e9e
Shija-cmd <shijajuma@protonmail.com>
2024-08-09 12:52:08 +0000

    update

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """The code is nearly identical to wait_n except task_wait_random is being called."""
+    """The code is nearly identical to wait_n except 
+       task_wait_random is being called."""
     futures = [task_wait_random(max_delay) for _ in range(n)]
     futures = asyncio.as_completed(futures)
     delays = [await future for future in futures]
